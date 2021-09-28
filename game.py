b57799a4a799c204bc4f27597de0a953a206ff89
Amos Ilmonen <ilmonen.amos@gmail.com>
2021-09-28 12:19:47 +0300

    Updated
    Added more art states
    Added word selection (optional)
    Added guessed characters

--- game.py
+++ game.py
@@ -59,12 +59,12 @@ word_library = [
 ]
 # Hangman art credit: https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
 HANGMANPICS = ['''
-       
-       
-       
-       
-       
-       
+
+
+
+
+
+
 =========''','''
       +
       |
@@ -73,54 +73,68 @@ HANGMANPICS = ['''
       |
       |
 =========''','''
+      +
+      |
+      |
+      |
+      |
+     /|
+=========''','''
+      +
+      |
+      |
+      |
+      |
+     /|\\
+=========''','''
   +---+
   |   |
       |
       |
       |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
       |
       |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
   |   |
       |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
  /|   |
       |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
  /|\  |
       |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
  /|\  |
  /    |
-      |
+     /|\\
 =========''', '''
   +---+
   |   |
   O   |
  /|\  |
  / \  |
-      |
+     /|\\
 =========''']
 
 
@@ -134,16 +148,27 @@ class hangman_game():
             self.main_textvar.set("-" * self.word_length)
             self.hang_textvar = GUI.StringVar()
             self.hang_textvar.set(HANGMANPICS[0])
+            self.guessed_textvar = GUI.StringVar()
             self.lives = len(HANGMANPICS) - 1
+            self.guessed_chars = []
  
         def lose_life(self):
             self.lives -= 1
-            print(str(self.lives))
             self.update_art()
+
+        def update_guessed(self):
+            self.guessed_textvar.set("Guessed: " + ", ".join(self.guessed_chars))
+
+        def add_wrong(self, char):
+            self.guessed_chars.append(char)
+            print(str(self.guessed_chars))
+            self.update_guessed()
+
                 
 
         def update_art(self):
             self.hang_textvar.set(HANGMANPICS[len(HANGMANPICS) - 1 - self.lives])
+
 
     def exit_game(self):
         self.game_window.destroy()
@@ -163,9 +188,10 @@ class hangman_game():
             messagebox.showwarning("GAME OVER", "You lose!")
             self.exit_game()
 
-    def wrong_guess(self):
+    def wrong_guess(self, char):
         print("Guess wrong")
         self.gamedata.lose_life()
+        self.gamedata.add_wrong(char)
 
     def replace_char(self, cur_string, new_char, index):
         return cur_string[:index] + new_char + cur_string[index + 1:]
@@ -179,26 +205,31 @@ class hangman_game():
     def guess_character(self):
         current_char = ""
         current_guess = self.text_entry.get()
-        if current_guess == "":
+        if (current_guess == ""):
             print("Invalid guess")
+            self.clear_entry()
             return
         else:
-            current_char = current_guess[0]
+            current_char = current_guess[0].lower()
+        if current_char in self.gamedata.guessed_chars:
+            print("Already guessed")
+            self.clear_entry()
+            return
         guess_length = len(current_char)
         _wordChosen = self.gamedata.chosen_word
         _currentString = self.gamedata.main_textvar.get()
         print("Guessed: " + current_char)
-        if current_char.lower() in _wordChosen.lower():
+        if current_char in _wordChosen.lower():
             guess_count = 0
             index_of = _wordChosen.find(current_char)
             for i in range(len(_wordChosen)):
-                if _wordChosen[i].lower() == current_char.lower():
+                if _wordChosen[i].lower() == current_char:
                     _currentString = self.replace_char(_currentString, _wordChosen[i], i)
             print("Guess end: " + _currentString)
             self.gamedata.main_textvar.set(_currentString)
         else:
             #Wrong guess
-            self.wrong_guess()
+            self.wrong_guess(current_char)
         self.check_victory()
         self.check_gameover()
         self.clear_entry()
@@ -206,19 +237,32 @@ class hangman_game():
     def init_elements(self):
         self.hang_label = GUI.Label(textvariable=self.gamedata.hang_textvar)
         self.word_label = GUI.Label(textvariable=self.gamedata.main_textvar)
+        self.guessed_label = GUI.Label(textvariable=self.gamedata.guessed_textvar)
         self.text_entry = GUI.Entry()
         self.submit_button = GUI.Button(text="Guess character", command=self.guess_character)
         self.hang_label.pack()
         self.word_label.pack()
         self.text_entry.pack()
+        self.guessed_label.pack()
         self.submit_button.pack()
 
-    def __init__(self):
+    def __init__(self, input_word):
+        word = ""
+        if input_word == False:
+            word = random.choice(word_library)
+        else:
+            word = input_word
         self.init_window()
-        self.gamedata = self.game_data(random.choice(word_library))
+        self.gamedata = self.game_data(word)
         self.init_elements()
         self.game_window.mainloop()
 
 
+def main():
+    print("Select word (Leave empty for random):")
+    word_input = input()
+    if word_input == "":
+        word_input = False
+    game_instance = hangman_game(word_input)
 
-game_instance = hangman_game()
+main()
